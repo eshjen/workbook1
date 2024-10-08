96aca7cf765e768478bdd01c1e1a37dea294c46c
Eshei Jenkins <eshjenk@gmail.com>
2024-10-08 15:48:31 -0400

    area of circle

---
+++ exercises/MathScripts/area_of_circle.js
@@ -0,0 +1,6 @@
+"use script";
+var radius = 4;
+var areaOfCircle = Math.PI * (radius ** 2);
+console.log(areaOfCircle);
+var message = "The area of a circle with radius " + 4 + " is areaOfCircle";
+console.log(message);
