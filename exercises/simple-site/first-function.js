f3dcfe0f05fc6f250df912914ef9051286ada8b2
Eshei Jenkins <eshjenk@gmail.com>
2024-10-21 09:21:07 -0400

    commit

---
+++ exercises/simple-site/first-function.js
@@ -0,0 +1,34 @@
+"use strict";
+function test() {
+  console.log("hey");
+  console.log("hi");
+  console.log("hello");
+}
+
+test();
+
+function Eshei() {
+  console.log("max");
+  console.log("fish");
+  console.log("turtles");
+}
+Eshei();
+
+function Python() {
+  console.log("HTML");
+  console.log("CSS");
+  console.log("JavaScript");
+}
+Python();
+
+function breakfast() {
+  console.log("eggs");
+  console.log("bacon");
+  console.log("pancakes");
+}
+breakfast();
+
+function greetMe() {
+  console.log("hola");
+}
+greetMe();
